bca25a223a5ec1394012f5f63e06063710a1258f
Michael Franklin <mf021036@gmail.com>
2024-09-28 19:51:00 -0600

    Animal class is set up

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,19 @@
+
 public class Animal {
+    protected String name;
+
+    //constructor
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    //method to be overwritten
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+
+    // Getter for the name
+    public String getName() {
+        return name;
+    }
 }
