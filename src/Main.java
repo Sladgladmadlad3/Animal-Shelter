e5239b969abd315cdad6a9a6dbaf8898dea2064b
Michael Franklin <mf021036@gmail.com>
2024-09-28 19:31:48 -0600

    Added two print statements to introduce

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the Animal Shelter Management System");
+        System.out.println("\nAdding animals to the shelter...");
+
     }
 }
